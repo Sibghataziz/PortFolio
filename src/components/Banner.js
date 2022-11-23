fe4d76a178d0b1bd8ab2bbb6a96a3894962626f0
sibghataziz <sibghat.aziz98@gmail.com>
2022-11-23 11:47:00 +0530

    profile pic change

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import sibghat from "../assets/img/sibghat.png";
+import sibghat from "../assets/img/sibghat.jpg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -73,7 +73,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={sibghat} alt="Header Img" style={{width:"400px", height:"400px"}}/>
+                  <img src={sibghat} alt="Header Img" style={{width:"400px", height:"500px"}}/>
                 </div>}
             </TrackVisibility>
           </Col>
